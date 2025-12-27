3e7382012d97dbcd03ea133f49bb11113d1506b7
Duc Nguyen <minhduc140583@gmail.com>
2025-12-27 17:31:01 +0700

    Refactor ContentUseCase

--- service/content/index.ts
+++ service/content/index.ts
@@ -1,4 +1,4 @@
-import { Log, Result, SearchUseCase, StringMap } from "onecore"
+import { Log, SearchWriter, StringMap } from "onecore"
 import { buildMap, DB, SqlSearchWriter } from "query-core"
 import { Content, ContentFilter, contentModel, ContentRepository, ContentService } from "./content"
 import { ContentController } from "./controller"
@@ -6,36 +6,27 @@ import { ContentController } from "./controller"
 export * from "./controller"
 
 export class SqlContentRepository extends SqlSearchWriter<Content, ContentFilter> implements ContentRepository {
-  constructor(protected db: DB) {
+  constructor(db: DB) {
     super(db, "contents", contentModel)
     this.map = buildMap(contentModel)
   }
   map?: StringMap
   load(id: string, lang: string): Promise<Content | null> {
-    return this.db.query<Content>(`select * from contents where id = ${this.db.param(1)} and lang = ${this.db.param(2)}`, [id, lang], this.map).then((contents) => {
+    return this.query<Content>(`select * from contents where id = ${this.param(1)} and lang = ${this.param(2)}`, [id, lang], this.map).then((contents) => {
       return !contents || contents.length === 0 ? null : contents[0]
     })
   }
   delete(id: string, lang: string): Promise<number> {
-    return this.db.exec(`delete from contents where id = ${this.db.param(1)} and lang = ${this.db.param(2)}`, [id, lang])
+    return this.exec(`delete from contents where id = ${this.param(1)} and lang = ${this.param(2)}`, [id, lang])
   }
 }
 
-export class ContentUseCase extends SearchUseCase<Content, ContentFilter> implements ContentService {
+export class ContentUseCase extends SearchWriter<Content, ContentFilter> implements ContentService {
   constructor(protected repository: ContentRepository) {
     super(repository)
   }
   load(id: string, lang: string): Promise<Content | null> {
     return this.repository.load(id, lang)
-  }
-  create(content: Content): Promise<Result<Content>> {
-    return this.repository.create(content)
-  }
-  update(content: Content): Promise<Result<Content>> {
-    return this.repository.update(content)
-  }
-  patch(content: Partial<Content>): Promise<Result<Content>> {
-    return this.repository.patch(content)
   }
   delete(id: string, lang: string): Promise<number> {
     return this.repository.delete(id, lang)
