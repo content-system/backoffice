a206787d5e296edd8b9b1a495588f3890e73937a
Duc Nguyen <minhduc140583@gmail.com>
2026-05-17 22:22:32 +0700

    Upgrade theme

---
+++ service/contact/repository.ts
@@ -0,0 +1,10 @@
+import { DB } from "onecore"
+import { Repository } from "sql-core"
+import { Contact, ContactFilter, contactModel, ContactRepository } from "./contact"
+export * from "./controller"
+
+export class SqlContactRepository extends Repository<Contact, string, ContactFilter> implements ContactRepository {
+  constructor(db: DB) {
+    super(db, "contacts", contactModel)
+  }
+}
